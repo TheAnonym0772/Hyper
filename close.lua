32ec8b18f393fd746233d35454df5984a9ee43f4
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2024-11-11 05:52:04 +0700

    Create close.lua

---
+++ close.lua
@@ -0,0 +1,34 @@
+local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local TextLabel = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0, 0, 0, 74)
+ImageButton.Size = UDim2.new(0, 43, 0, 43)
+ImageButton.Image = "rbxassetid://96915647894802"
+ImageButton.MouseButton1Down:connect(function()
+	game:service('VirtualInputManager'):SendKeyEvent(true, "L", false, game)
+	game:service('VirtualInputManager'):SendKeyEvent(false, "L", false, game)
+end)
+
+
+TextLabel.Parent = ImageButton
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Size = UDim2.new(0, 43, 0, 43)
+TextLabel.Font = Enum.Font.Highway
+TextLabel.Text = "GUI"
+TextLabel.TextColor3 = Color3.fromRGB(25, 25, 25)
+TextLabel.TextSize = 14.000
+TextLabel.TextTransparency = 0.510
+print("Suncc")
